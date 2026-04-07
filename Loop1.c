a3c7ceaee7ba7f5750eb12213a39fa29516816a9
nivshankar <nivshankar9@gmail.com>
2026-04-07 17:21:04 +0530

    Add files via upload

--- Loop1.c
+++ Loop1.c
@@ -1,57 +1,30 @@
 #include<stdio.h>
 int main()
-{
+{//while loop 
 	int i=1,n;
 	printf("Enter n:\n");
 	scanf("%d",&n);
 	while(i<=n)
 	{
+		if(i%2!=0)
 		printf("\n%d",i);
+		else
+		printf("\n%d",i*i);
 		i++;
 	}
+	
 }
 /*
 Output:
 Enter n:
-39
+8
 
 1
-2
-3
 4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
+3
 16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
+5
 36
-37
-38
-39
+7
+64
 */
