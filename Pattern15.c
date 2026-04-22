d29cfeb2e8bff2f6b6c014e946921e7a0b7cedfd
nivshankar <nivshankar9@gmail.com>
2026-04-22 18:57:36 +0530

    Add files via upload

---
+++ Pattern15.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		printf("%d ",j);
+		for(j=5;j>i;j--)
+		printf("    ");
+		for(j=i;j>=1;j--)
+		printf("%d ",j);
+		printf("\n");
+	}
+}
+/*
+Output:
+1 2 3 4 5 5 4 3 2 1
+1 2 3 4     4 3 2 1
+1 2 3         3 2 1
+1 2             2 1
+1                 1
+
+*/
